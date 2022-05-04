1f3d3333f4c7ff1a00f2710a4d9ef5d88d9a0a34
Aleksey-Sarafanov <81502379+Aleksey-10081967@users.noreply.github.com>
2022-05-04 15:37:08 +0300

    Add files via upload

---
+++ pgbadger/conf_file/install_service.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cp report-pgs-logs.sh /usr/bin/
+chmod 755 /usr/bin/report-pgs-logs.sh
+
+cp report-pgs-logs.service /etc/systemd/system/
+cp report-pgs-logs.timer /etc/systemd/system/
+cp report-pgs-logs.cfg   /etc/default/
+
+# Создание папки для размещения отчетов
+# Местонахождение и название папки должно полностью совпадать со значением переменной DIR_OUTPUT фаала  report-pgs-logs.cfg
+mkdir -p /tmp/report
+
+# Назначение  владельцем postgres созданной папки. Неоходимо т.к.  report-pgs-logs.service выполняется под postgres
+chown -R postgres:postgres /tmp/report
+
+systemctl daemon-reload
+systemctl enable report-pgs-logs.timer
+systemctl enable report-pgs-logs.service
+systemctl start report-pgs-logs.timer
+systemctl start report-pgs-logs.service
